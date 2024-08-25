8935b8d35c322421bda607db9182403aead2af56
Zoe Forbes <60221117+z-forbes@users.noreply.github.com>
2024-08-24 20:51:05 -0700

    add key info

--- src/main/java/program/extras/Confidential.java
+++ src/main/java/program/extras/Confidential.java
@@ -6,14 +6,18 @@ package program.extras;
  */
 
 public class Confidential {
+    /* AWS Keys */ // these aren't used when self-hosted
     public static final String BucketData_accessKey = "";
     public static final String BucketData_secretKey = "";
 
+    /* Spotify Web API keys */
     public static final String SpotifyAuth_refresh = "";
     public static final String SpotifyAuth_clientID = "";
     public static final String SpotifyAuth_clientSecret = "";
 
+    /* Google Maps Platform Keys */
+    // Geocoding Key
     public static final String CountryFromPlace_API_KEY = "";
-
+    // GeoCharts
     public static final String ReturnObject_API_KEY = "";
 }
